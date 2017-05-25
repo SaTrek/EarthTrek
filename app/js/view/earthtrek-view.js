6d3b14c2e6aa3df4cf0bc259996921d837067265
Alejandro Sánchez <alejandro.sanchez.trek@gmail.com>
2017-05-25 04:57:40 -0300

    UI Panel Improvements

---
+++ app/js/view/earthtrek-view.js
@@ -0,0 +1,19 @@
+/**
+ * @class EarthTrekView
+ * @module EarthTrek
+ * @author SATrek
+ * @author Alejandro Sanchez <alejandro.sanchez.trek@gmail.com>
+ * @description EarthTrek - NASA Space Apps 2017 25 MAY 2017.
+ */
+//require("amd-loader");
+define([
+    'jquery',
+    'bootstrap'
+], function () {
+
+    function EarthTrekView(viewer, options) {
+        this.viewer = viewer;
+    }
+
+    return EarthTrekView;
+});
