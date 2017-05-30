b879f93aee5a52bb58938ac0f35d17f331810e4f
Alejandro Sanchez <alejandro.sanchez.trek@gmail.com>
2017-05-30 18:05:47 -0300

    Webpack Test

--- app/js/main.js
+++ app/js/main.js
@@ -5,19 +5,14 @@
  * @author Alejandro Sanchez <alejandro.sanchez.trek@gmail.com>
  * @description EarthTrek - NASA Space Apps 2017 - 23 APR 2017.
  */
-define([
-    'jquery',
-    'earthtrek'
-], function ($, EarthTrek) {
 
-    var earthTrek = new EarthTrek({
-        startTime: Date.UTC(1999, 1, 1),
-        endTime: Date.now(),
-        initialTime: Date.now(),
-        mainContainer: 'main-container',
-        frequency: 50,
-        maxDistanceCamera: 10000000000 //10,000,000,000 meters
-    });
-    earthTrek.createViewer();
-    earthTrek.init();
+var earthTrek = new EarthTrek({
+    startTime: Date.UTC(1999, 1, 1),
+    endTime: Date.now(),
+    initialTime: Date.now(),
+    mainContainer: 'main-container',
+    frequency: 50,
+    maxDistanceCamera: 10000000000 //10,000,000,000 meters
 });
+earthTrek.createViewer();
+earthTrek.init();
