c37d191ad76ae0523c7c563e23d2fa584bbe6c81
Alejandro Sanchez <alejandro.sanchez.trek@gmail.com>
2017-06-07 22:01:16 -0300

    IE11 supported

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 module.exports = {
     context: path.join(__dirname, '../src'),
-    entry: "./main.js",
+    entry: ['core-js/fn/promise',  './main.js'],
     output: {
         path: "./public",
         sourcePrefix: "",
